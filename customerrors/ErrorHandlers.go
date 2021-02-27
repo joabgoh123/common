7d2cc24f17b6b80e21aca624e829ca00a7e268ba
Joab <joabgoh123@gmail.com>
2021-02-27 17:20:10 +0700

    Added Base Error Handler

--- customerrors/ErrorHandlers.go
+++ customerrors/ErrorHandlers.go
@@ -1,12 +1,15 @@
 package customerrors
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 type BaseErrorHandler func(http.ResponseWriter, *http.Request) error
 
 func (fn BaseErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := fn(w, r)
 	if err != nil {
-
+		fmt.Println("Hello world!")
 	}
 }
