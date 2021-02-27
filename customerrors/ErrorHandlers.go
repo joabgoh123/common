4aff1f9c26a0f475a8042456e5719a11dcbe3cd1
Joab <joabgoh123@gmail.com>
2021-02-27 19:21:31 +0700

    Base Handler Wrapper

--- customerrors/ErrorHandlers.go
+++ customerrors/ErrorHandlers.go
@@ -7,6 +7,10 @@ import (
 
 type BaseErrorHandler func(http.ResponseWriter, *http.Request) error
 
+func BaseErrorHandlerWrapper(fn func(http.ResponseWriter, *http.Request) error) BaseErrorHandler{
+	return BaseErrorHandler(fn)
+}
+
 func (fn BaseErrorHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	err := fn(w, r)
 	if err != nil {
